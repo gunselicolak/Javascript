a18d2ac71c42c7ff5b809ca01934c249b38803cb
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-02-16 15:10:30 +0300

    Add files via upload

---
+++ Javascript (JS) Giriş/nots.js
@@ -0,0 +1,30 @@
+// document write - document writeln
+    
+    document.write("Github'a hoşgeldiniz !! <br>");
+    document.write("Javascript - CSS - HTML <br>");
+    
+    document.write("PHP");
+    document.write("Javascript");
+    document.write("Bootstrop <br>");
+    
+    document.writeln("PHP");
+    document.writeln("Javascript");
+    document.writeln("Bootstrop <br>");
+    
+    var g = "gunseli";
+
+    document.write(g);
+
+// alert kullanımı
+
+    alert("Hoşgeldin :)");
+
+// prompt kullanımı
+
+    var sayi1 = prompt("Bir sayı girin");
+    var sayi2 = prompt("Bir sayı girin");
+
+    topla = parseInt(sayi1) + parseInt(sayi2);
+
+    document.getElementById("sonuc").innerHTML = topla;
+
