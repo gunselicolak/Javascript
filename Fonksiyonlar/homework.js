583f25299914cf838f12979bce93793019ed4c0b
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-02-28 22:32:39 +0300

    Update homework.js

--- Fonksiyonlar/homework.js
+++ Fonksiyonlar/homework.js
@@ -15,3 +15,30 @@
 
     ad yaşı yaş
 */
+
+
+
+
+
+
+
+
+/*
+    Homework 3
+    Girilen parametre değerine kadar olan 
+    sayıları toplayan function yazın.
+ */
+
+var i = 0;
+var sonuc = 0;
+
+function toplayan(sayi){
+    if(i<sayi){
+        sonuc += i;
+        i++;
+        document.write((sonuc+i)+"<br>");
+        toplayan(sayi);
+    }
+}
+
+toplayan(3);
