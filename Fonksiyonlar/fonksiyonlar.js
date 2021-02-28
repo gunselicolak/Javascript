30e425ccb7a27bc6bb2dd041147d9b350eb85917
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-02-28 22:15:13 +0300

    Add files via upload

---
+++ Fonksiyonlar/fonksiyonlar.js
@@ -0,0 +1,25 @@
+// Fonksiyonlar
+/* Fonksiyon nedir? 
+    Bir işlevi yerine getiren kod bloklarıdır. 
+    Hazır ve kullanıcı tanımlı olarak 2 temel sınıftan oluşur.
+    (Parametresiz/Parametreli) 
+*/
+// Parametresiz Fonksiyonlar
+/*
+function topla(){
+    var a = 10, b = 20;
+    
+    return sonuc = a + b;
+}
+
+document.write(topla());
+*/
+
+// Parametreli Fonksiyonlar
+/*
+function topla(sayi1,sayi2){
+    document.write(sayi1+sayi2);
+}
+
+topla(5,5);
+*/
---
+++ Fonksiyonlar/homework.js
@@ -0,0 +1,17 @@
+/*
+    Homework 1
+    Doğum yılınızı girdiğinizde size yaşınızı söyleyen 
+    fonsiyonu yazın..
+*/
+
+
+
+
+
+
+/*
+    Homework 2
+    Fonksiyona ad ,yil size şöyle karşılama
+
+    ad yaşı yaş
+*/
