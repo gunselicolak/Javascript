9cf6e4abb08db2d37317947659d0cf651f098b7f
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-02-28 22:26:00 +0300

    Update fonksiyonlar.js

--- Fonksiyonlar/fonksiyonlar.js
+++ Fonksiyonlar/fonksiyonlar.js
@@ -4,7 +4,7 @@
     Hazır ve kullanıcı tanımlı olarak 2 temel sınıftan oluşur.
     (Parametresiz/Parametreli) 
 */
-// Parametresiz Fonksiyonlar
+// Parametresiz Fonksiyon
 /*
 function topla(){
     var a = 10, b = 20;
@@ -15,11 +15,50 @@ function topla(){
 document.write(topla());
 */
 
-// Parametreli Fonksiyonlar
+// Parametreli Fonksiyon
 /*
 function topla(sayi1,sayi2){
     document.write(sayi1+sayi2);
 }
 
 topla(5,5);
+*/
+
+/*
+function carp(a,b){
+    return sonuc = a*b;
+
+    // return sonuc; => bir işlemin yürütülmesini durur ama 
+                        // bu işlemden dönen değeri döndürür. 
+}
+
+document.write(carp(5,5));
+*/
+
+// Fonksiyon Geçişleri
+/*
+var islem1 = function(yap1){
+    yap1(20+10);
+}
+
+var islem2 = function(yap2){
+    document.write(10+yap2);
+}
+
+islem1(islem2);
+*/
+
+// Recursive(Tekrarlamalı) Fonkiyonlar
+/*
+var i = 0;
+
+function tekrarla(param){
+    if(i<param){
+        i++;
+        document.write(i+"<br>");
+        tekrarla(param);
+    }
+}
+
+tekrarla(5);
 */
