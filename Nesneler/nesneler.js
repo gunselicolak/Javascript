681a44ad558e3c67e9e7ac7bcb32e4ec7008e2eb
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-03-10 00:07:02 +0300

    Update nesneler.js

--- Nesneler/nesneler.js
+++ Nesneler/nesneler.js
@@ -14,6 +14,11 @@
 
     console.log(araba);
 
+    document.write(araba.marka+"<br>");
+    document.write(araba.renk+"<br>");
+    document.write(araba.yil+"<br>");
+    document.write(araba.km+"<br>");
+
     // functionla nesnee tanımlama 
 
     function araba(marka,renk,yil,km)
@@ -29,4 +34,29 @@
 
     console.log(araba1);
     console.log(araba2);
+
+    document.write(araba1.markasi+"<br>");
+    document.write(araba1.rengi+"<br>");
+    document.write(araba1.yili+"<br>");
+    document.write(araba1.kmsi+"<br>");
+
+    document.write("<br>");
+    
+    document.write(araba2.markasi+"<br>");
+    document.write(araba2.rengi+"<br>");
+    document.write(araba2.yili+"<br>");
+    document.write(araba2.kmsi+"<br>");
+
+//
+    var insan = new Object();
+
+    insan.isim = "bilge";
+    insan.soyad = "seli";
+    insan.yas = 19;
+    
+    console.log(insan);
+
+    document.write(insan.isim);
+    document.write(insan.soyad);
+    document.write(insan.yas);
 
