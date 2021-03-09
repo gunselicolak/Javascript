ed9262340f0c55f2b53debe3057d6f6e68bc438e
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-03-09 23:44:06 +0300

    Add files via upload

---
+++ Nesneler/nesneler.js
@@ -0,0 +1,27 @@
+/*
+    Nesne nedir ?
+    nesnelerin özellileriyle oluşan değerlere method denir.
+    js nesneye dayalı programlama dilidir.
+
+*/
+
+    var araba = {
+        marka ="range rover";
+        renk="siyah";
+        yil ="2018";
+        km="45000";
+    }
+
+    console.log(araba);
+
+    // functionla nesnee tanımlama 
+
+    function araba(marka,renk,yil,km)
+    {
+        this.markasi = marka;
+        this.rengi = renk;
+        this.yili = yil;
+        this.kmsi = km;
+    }
+
+    var araba1 = new araba("")
