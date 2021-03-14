6e92c7a743c350a7e4c55c2ebb735e666181c687
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-03-14 22:09:16 +0300

    Add files via upload

---
+++ DOM Events/Mouse Olayları/toplamHesap.js
@@ -0,0 +1,11 @@
+/* 2 input oluşturun bu iki inputa  girilen 
+    sayıların toplamını veren  projeyi yapın
+*/
+
+function hesapla(){
+    var sayi1 =  parseInt(document.getElementById("sayi1").value);
+    var sayi2 =  parseInt(document.getElementById("sayi2").value);
+
+    document.getElementById("sonuc").innerHTML=sayi1+sayi2;
+    
+}
