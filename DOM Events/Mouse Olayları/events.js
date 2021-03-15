d875cd10e1597ccb7b7ba1da3fc78457eba6387c
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-03-15 22:43:06 +0300

    Add files via upload

---
+++ DOM Events/Mouse Olayları/events.js
@@ -0,0 +1,26 @@
+function ciftTikla(){
+    alert("Çift Tıklandı");
+}
+function sagTikla(){
+    alert("Sağ Tıklandı");
+}
+function kontrol(){
+    var k = confirm("Onaylıyor musunuz?");
+
+    if(k==true){
+        document.getElementById("cevap").innerHTML="Onay verildi.";
+    }
+    else{
+        document.getElementById("cevap").innerHTML="Onay verilmedi.";
+    }
+}
+function uzerineGeldi(){
+    alert("Üzerine Geldi");
+}
+function uzerindenGitti(){
+    console.log("Gitti..");
+}
+function yuklendi(){
+    console.log("Sayfa yüklendi...")
+}
+//window.onload=yuklendi;
