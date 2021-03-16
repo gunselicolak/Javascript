af58e9867f5bfc0f8ea510f1d372b3b4d2c08210
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-03-17 00:43:26 +0300

    Add files via upload

---
+++ Hata Yönetimi/hataYonetimi.js
@@ -0,0 +1,11 @@
+
+        // Try Catch
+
+        var a = 10;
+
+        try{
+            document.write(a+b)
+        }
+        catch(err){
+            document.write("Hatalı İşlem: " +err)
+        }
