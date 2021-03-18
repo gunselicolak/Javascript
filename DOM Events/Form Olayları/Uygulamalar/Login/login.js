bac131587d679edb518a944f2626d60959da01b2
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-03-18 22:30:08 +0300

    Update login.js

--- DOM Events/Form Olayları/Uygulamalar/Login/login.js
+++ DOM Events/Form Olayları/Uygulamalar/Login/login.js
@@ -2,3 +2,14 @@
    alt tarafa adınız altı karakterden az olamaz
    uyarısı gelsin
 */
+
+function adKontrol(){
+    
+    var e = document.getElementById("ad").value;
+    var say = e.length;
+
+    if(say<6)
+    {
+        document.getElementById("uyari1").innerHTML="Adınız en az 6 karakterden oluşmalı şimdi "+say+" karakter";
+    }
+}
