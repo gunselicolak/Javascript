1790bcbd981df795f022f8b0d98517a5deecd6b5
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-03-18 21:48:44 +0300

    Update events.js

--- DOM Events/Klavye Olayları/events.js
+++ DOM Events/Klavye Olayları/events.js
@@ -1,7 +1,16 @@
 i=0;
-function islem(){
+function islem1(){
     //console.log("Olay Oldu");
     i+=1;
 
-    document.getElementById("cikti").innerHTML=i;
+    document.getElementById("cikti1").innerHTML=i;
+}
+j=0;
+function islem2(){
+    j+=1;
+    document.getElementById("cikti2").innerHTML=j;
+
+    if(j>=160){
+        document.getElementById("cikti2").innerHTML=i+" karakter aşımı yaşandı.";
+    }
 }
