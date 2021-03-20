ae5a9a070ebeed0c64b73553e1b9756796e65d01
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-03-20 21:46:07 +0300

    Add files via upload

---
+++ BOM/open-close.js
@@ -0,0 +1,10 @@
+function ac(){
+    var yeniPencere = window.open("", "yeniPencere", "width=250, height=200");
+    yenipencere.document.write("<h1>yeniPencere</h1>");
+
+}
+function islem(){
+    var sayi1 = document.getElementById("sayi1").value;
+    var sayi2 = document.getElementById("sayi2").value;
+
+}
